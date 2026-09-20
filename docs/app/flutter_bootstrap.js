cbb29d041302f640d0e36237445d762c726413b7
Alfonso <sage50alcatic@gmail.com>
2026-09-20 20:09:52 +0000

    docs: refresh browser version to 0.3.16+22

--- docs/app/flutter_bootstrap.js
+++ docs/app/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"06a2e2a110089dff50fe635cffd2a61e1b24fb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "656453457" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1888207821" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
